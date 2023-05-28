def4789959a530da40845fe6d2e5959ed7b3506c
brunocaxias <bccaxias03.bc@gmail.com>
2023-05-27 21:44:18 -0300

    ate a aula 10

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
 import { CoursesRoutingModule } from './courses-routing.module';
+import { CoursesComponent } from './courses/courses.component';
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CoursesComponent
+  ],
   imports: [
     CommonModule,
-    CoursesRoutingModule
+    CoursesRoutingModule,
+    AppMaterialModule
   ]
 })
 export class CoursesModule { }
--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../models/course';
+import { CoursesService } from '../services/courses.service';
+import { Observable} from 'rxjs';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit{
+
+  courses$:  Observable <Course[]>;
+  displayedColumns = ['name', 'category'];
+
+  constructor(private coursesService : CoursesService){
+    this.courses$ = this.coursesService.list();
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented. ');
+  }
+
+
 
 }
