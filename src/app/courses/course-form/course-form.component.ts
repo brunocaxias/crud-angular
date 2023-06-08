b791e2ca1b25af3361772e15b6bc8f324ca3a7f5
brunocaxias <bccaxias03.bc@gmail.com>
2023-06-08 18:47:17 -0300

    ate aula 20

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { CoursesService } from '../services/courses.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-course-form',
@@ -7,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseFormComponent implements OnInit {
 
-  constructor(){}
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar){
+    this.form = this.formBuilder.group({
+      name: [null],
+      category: [null]
+    });
+  }
 
   ngOnInit(): void {
+  }
 
+  onSubmit() {
+    console.log('onSubmit');
+    this.service.save(this.form.value)
+      .subscribe({
+        next: data => console.log(data),
+        error: error => this.onError()
+      });
+  }
+
+  onCancel(){
+    console.log('onCancel');
+  }
+
+  private onError(){
+    this.snackBar.open('Erro ao salvar curso','',{duration:5000});
   }
 
 }
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -13,11 +13,16 @@ export class CoursesService {
   constructor(private httpClient: HttpClient) { }
 
   list(){
-    return this.httpClient.get<Course[]>(this.API).pipe(
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
       first(),
       tap(courses =>console.log(courses))
     )
 
+  }
+
+  save(record: Course){
+    return this.httpClient.post<Course>(this.API, record).pipe(first());
   }
 
 }
